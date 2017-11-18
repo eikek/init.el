950ccdf13fd65c33cfa4ed43388181a56dd99ed3
Eike Kettner <eike.kettner@posteo.de>
2017-11-18 22:43:32 +0100

    Remove redundant email signature

--- init.el
+++ init.el
@@ -1371,9 +1371,7 @@
               (mu4e-sent-folder . "/eknet/Sent")
               (mu4e-trash-folder . "/eknet/Trash")
               (mu4e-drafts-folder . "/eknet/Drafts")
-              (mu4e-compose-signature . ,(concat "gpg: AD7AC35E\n"
-                                                 "finger print: 137F BB0B 1639 D25F DC5D"
-                                                 " E59C B412 C5F5 AD7A C35E"))
+              (mu4e-compose-signature . ,(concat "gpg: AD7AC35E\nhttps://eikek.github.io/sharry/"))
               (smtpmail-smtp-server . ,(my/password-store-get-key "email/eknet.org" "mailhost"))
               (smtpmail-smtp-user . ,(my/password-store-get-user "email/eknet.org"))
               (smtpmail-smtp-service . 25)
@@ -1395,9 +1393,7 @@
               (mu4e-sent-folder . "/posteo/Sent")
               (mu4e-trash-folder . "/posteo/Trash")
               (mu4e-drafts-folder . "/posteo/Drafts")
-              (mu4e-compose-signature . ,(concat "gpg: AD7AC35E\n"
-                                                 "finger print: 137F BB0B 1639 D25F DC5D"
-                                                 " E59C B412 C5F5 AD7A C35E"))
+              (mu4e-compose-signature . ,(concat "gpg: AD7AC35E\nhttps://eikek.github.io/sharry/"))
               (smtpmail-smtp-server . ,(my/password-store-get-key "email/posteo.de" "mailhost"))
               (smtpmail-smtp-user . ,(my/password-store-get-user "email/posteo.de"))
               (smtpmail-smtp-service . 587)
