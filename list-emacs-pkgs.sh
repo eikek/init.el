f009d674939010e29aa46a13a23ba22d18e91333
Eike Kettner <eike.kettner@posteo.de>
2018-09-05 00:14:50 +0200

    bed

--- list-emacs-pkgs.sh
+++ list-emacs-pkgs.sh
@@ -3,4 +3,4 @@
 echo ":a import <nixpkgs> {}
 contains = s: (lib.hasPrefix \"$1\" s) || (lib.hasSuffix \"$1\" s)
 names = (builtins.attrNames emacsPackages) ++ (builtins.attrNames emacs25PackagesNg)
-builtins.filter contains names" | nix-repl
+builtins.filter contains names" | nix repl
