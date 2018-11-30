cd8ad261dedffe0d58a14cd4599c76f9cc74dfa0
Eike Kettner <eike.kettner@posteo.de>
2018-11-30 22:19:00 +0100

    Fixing org-expenses build
    It used to work with `trivialBuild` and the required packages in
    `packageRequires`. But it doesn't work anymore. When looking at it, I
    found the lisp files deeper below the `site-lisp` directory within
    dash (for example), but the emacs load-path had the path a few levels
    too much up.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,7 @@
 { pkgs, emacsPackagesNg }:
 
 [ (import ./helm-org-clock.nix {inherit pkgs emacsPackagesNg;})
-#  (import ./org-expenses.nix {inherit pkgs emacsPackagesNg;})
-(import ./test-req {inherit pkgs emacsPackagesNg;})
+  (import ./org-expenses.nix {inherit pkgs emacsPackagesNg;})
   (import ./erosiond-theme.nix {inherit pkgs emacsPackagesNg;})
   (import ./colonoscopy-theme.nix {inherit pkgs emacsPackagesNg;})
   (import ./git-timemachine.nix {inherit pkgs emacsPackagesNg;})
--- pkgs/org-expenses.nix
+++ pkgs/org-expenses.nix
@@ -1,10 +1,10 @@
 { pkgs, emacsPackagesNg }:
 
-emacsPackagesNg.trivialBuild rec {
+pkgs.stdenv.mkDerivation rec {
 
   pname = "org-expenses";
-
   version = "0.0.1";
+  name = "${pname}-${version}";
 
   src = pkgs.fetchFromGitHub {
     owner = "eikek";
@@ -13,5 +13,24 @@ emacsPackagesNg.trivialBuild rec {
     sha256 = "14wy56lnfqnpp2l94n3yqdcarncs5nc23g0ak51ahi36aykl60gv";
   };
 
-  packageRequires = with emacsPackagesNg; [ dash s org ];
+  buildInputs = [ pkgs.emacs ];
+  propagatedUserEnvPkgs = with emacsPackagesNg; [ dash s org ];
+
+  unpackPhase = "true";
+
+  # It used to work with `trivialBuild' and `packageRequires' list but
+  # it doesn't do anymore. It seems as if the lisp files are stored
+  # deep down below the `site-lisp' dir, but only the `site-lisp' dir
+  # is in emacs load-path.
+  buildPhase = ''
+    cp -r $src/* .
+    LDASH=$(find ${emacsPackagesNg.dash}/share/emacs/site-lisp -type d | tail -n1)
+    LES=$(find ${emacsPackagesNg.s}/share/emacs/site-lisp -type d | tail -n1)
+    emacs -L . -L $LDASH -L $LES --batch -f batch-byte-compile *.el
+  '';
+
+  installPhase = ''
+    install -d $out/share/emacs/site-lisp
+    install *.el *.elc $out/share/emacs/site-lisp
+  '';
 }
