1d10bb3bbe00f4e22b6146409ebb48e29f21466a
Eike Kettner <eike.kettner@tundra.com>
2022-01-05 18:41:59 +0100

    update

--- lisp/work.el
+++ lisp/work.el
@@ -1,5 +1,14 @@
 ;;; Some work related stuff
+;;; Code:
 
+(defun work/clock-toggle (&optional dir)
+  "Find the current log file in DIR and clocks in or out depending on current state."
+  (interactive)
+  (let* ((logdir (or dir "~/org/tundra/log"))
+         (file (-last-item (sort (f-entries logdir
+                                            (lambda (p) (s-starts-with-p "20" (f-base p)))) 'string<))))
+    file))
 
+;(work/clock-toggle)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (provide 'work)
