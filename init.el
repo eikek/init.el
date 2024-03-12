93ff1b5fa05b4c758058e80da27a7e832db305ac
eikek <eike.kettner@posteo.de>
2024-03-12 01:17:08 +0100

    Use consult instead of ivy, disable eaf, envrc instead of direnv

--- init.el
+++ init.el
@@ -134,29 +134,170 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; ivy / counsel / swiper
 
-(use-package ivy
-  :demand t
-  :diminish (ivy-mode)
+;; (use-package ivy
+;;   :demand t
+;;   :diminish (ivy-mode)
+;;   :config
+;;   (ivy-mode 1)
+;;   (setq ivy-height 15)
+;;   (setq ivy-use-selectable-prompt t)
+;;   (setq ivy-use-virtual-buffers t))
+
+;; (use-package swiper
+;;   :bind (("C-s" . swiper)
+;;          ("C-c C-r" . ivy-resume)))
+
+;; (use-package counsel
+;;   :bind (("C-x C-f" . counsel-find-file)
+;;          ("C-x b" . counsel-switch-buffer)
+;;          ("C-x C-b" . counsel-ibuffer)
+;;          ("M-x" . counsel-M-x)
+;;          ("C-x l" . counsel-locate)
+;;          ("C-h f" . counsel-describe-function)
+;;          ("C-h v" . counsel-describe-variable)
+;;          (:map counsel-find-file-map
+;;                ("C-." . counsel-up-directory))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; consult / embark / marginalia / vertico
+
+(use-package consult
+  :bind (;; C-c bindings in `mode-specific-map'
+         ("C-c M-x" . consult-mode-command)
+;;         ("C-c h" . consult-history)
+;;         ("C-c k" . consult-kmacro)
+         ("C-c m" . consult-man)
+         
+         ("C-c p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ("C-c p p" . consult-projectile-switch-project)
+         ("C-c p f" . consult-projectile-find-file)
+
+         ;;         ("C-c i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ;; C-x bindings in `ctl-x-map'
+ ;;        ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ;; Custom M-# bindings for fast register access
+         ;;         ("M-#" . consult-register-load)
+         ;;         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ;;         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ;; M-g bindings in `goto-map'
+         ("M-g e" . consult-flycheck)
+;;         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings in `search-map'
+         ("M-s d" . consult-find)                  ;; Alternative: consult-fd
+         ("M-s c" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s zL" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+
+  ;; Enable automatic preview at point in the *Completions* buffer. This is
+  ;; relevant when you use the default completion UI.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+
+  ;; Optionally configure the register formatting. This improves the register
+  ;; preview for `consult-register', `consult-register-load',
+  ;; `consult-register-store' and the Emacs built-ins.
+  (setq register-preview-delay 0.5
+        register-preview-function #'consult-register-format)
+
+  ;; Optionally tweak the register preview window.
+  ;; This adds thin lines, sorting and hides the mode line of the window.
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
   :config
-  (ivy-mode 1)
-  (setq ivy-height 15)
-  (setq ivy-use-selectable-prompt t)
-  (setq ivy-use-virtual-buffers t))
 
-(use-package swiper
-  :bind (("C-s" . swiper)
-         ("C-c C-r" . ivy-resume)))
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key "M-.")
+  ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-file-register
+   consult--source-recent-file consult--source-project-recent-file
+   ;; :preview-key "M-."
+   :preview-key '(:debounce 0.4 any))
 
-(use-package counsel
-  :bind (("C-x C-f" . counsel-find-file)
-         ("C-x b" . counsel-switch-buffer)
-         ("C-x C-b" . counsel-ibuffer)
-         ("M-x" . counsel-M-x)
-         ("C-x l" . counsel-locate)
-         ("C-h f" . counsel-describe-function)
-         ("C-h v" . counsel-describe-variable)
-         (:map counsel-find-file-map
-               ("C-." . counsel-up-directory))))
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; "C-+"
+
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  ;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
+
+  ;; By default `consult-project-function' uses `project-root' from project.el.
+  ;; Optionally configure a different project root function.
+  (autoload 'projectile-project-root "projectile")
+  (setq consult-project-function (lambda (_) (projectile-project-root)))
+)
+
+(use-package marginalia
+  :config
+  (marginalia-mode))
+
+(use-package vertico
+  :config
+  (vertico-mode))
+
+(use-package embark
+  :bind
+  (("M-o" . embark-act)
+   ("C-h B" . embark-bindings)))
+
+;; Optionally use the `orderless' completion style.
+(use-package orderless
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
+(use-package wgrep)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -485,19 +626,19 @@
 ;;; projectile
 
 (use-package projectile
-  :bind (("C-c pp" . ivy-projectile-switch-project))
+  :bind (("C-c pp" . consult-projectile-switch-project))
   :config
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   (setq projectile-indexing-method 'alien)
-  (setq projectile-completion-system 'ivy)
+  (setq projectile-completion-system 'auto)
   (setq projectile-remember-window-configs t)
   (setq projectile-find-dir-includes-top-level t)
   (setq projectile-mode-line-prefix " Ƥ"))
 
-(use-package counsel-projectile
-  :config
-  (counsel-projectile-mode)
-  (setq counsel-projectile-switch-project-action 'magit-status))
+;; (use-package counsel-projectile
+;;   :config
+;;   (counsel-projectile-mode)
+;;   (setq counsel-projectile-switch-project-action 'magit-status))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -1138,10 +1279,10 @@ rich-text version of what is assumed to be an org mode body."
 
 (use-package lsp-metals)
 
-(use-package lsp-ivy
-  :commands lsp-ivy-workspace-symbol
-  :bind (:map lsp-mode-map
-              ("M-s f" . lsp-ivy-workspace-symbol)))
+;; (use-package lsp-ivy
+;;   :commands lsp-ivy-workspace-symbol
+;;   :bind (:map lsp-mode-map
+;;               ("M-s f" . lsp-ivy-workspace-symbol)))
 
 (use-package lsp-metals
   :bind (:map lsp-mode-map
@@ -1197,14 +1338,6 @@ rich-text version of what is assumed to be an org mode body."
   :config
   (setq rustic-lsp-client 'lsp-mode)
   (setq rustic-format-on-save t))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; direnv
-
-(use-package direnv
-  :config
-  (direnv-mode))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -1474,6 +1607,11 @@ rich-text version of what is assumed to be an org mode body."
   :if (my/host-starts-with-p "staff")
   :demand t)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; envrc
+(use-package envrc
+  :config
+  (envrc-global-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; theme
@@ -1537,6 +1675,7 @@ The `VALUE' may be 0=transparent to 100=opaque."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; emacs application framework (eaf) - browser, pdf-viewer
 (use-package eaf
+  :disabled t
   :config
   (setq eaf-browser-default-search-engine "duckduckgo")
   (use-package eaf-browser
